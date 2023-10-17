b48e4738ec4fed2a872434e19262c34e0ddfd7cc
AntonyUsoltsev <usoltsev_anton1@mail.ru>
2023-10-17 16:29:54 +0700

    add huf, delta huf using numpy

--- constants.py
+++ constants.py
@@ -6,6 +6,23 @@ class ConstantNotFoundException(Exception):
 
 
 class Constants:
+    D_LAI: float
+    HI: float #three values from Таблица вычислений_НСО_районы_2022.xlsx
+    HMX: float
+    LAImx: float
+    PHU: float
+    RDMX: float
+    SNk: float
+    Tb: float
+    Topt: float
+    ad: float
+    СО2: float
+    ah1: float
+    ah2: float
+    bc1: float
+    bc2: float
+    bc3: float
+
     def __init__(self):
         constant_dict = {}
         df = pd.read_excel('./constants/constant_raion.xlsx')
@@ -19,8 +36,8 @@ class Constants:
 
         constants_to_check = ["D_LAI", "HI", "HMX", "LAImx", "PHU", "RDMX", "SNk", "Tb", "Topt", "ad", "СО2", "ah1",
                               "ah2", "bc1", "bc2", "bc3"]
-
-        for const in constants_to_check:
+        #  Constants.__dict__["__annotations__"].keys()
+        for const in Constants.__dict__["__annotations__"].keys():
             value = constant_dict.get(const)
             if value is not None:
                 setattr(self, const, value)

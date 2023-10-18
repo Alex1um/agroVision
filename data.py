8e62da391aefb22208fd53482e35766ac9e9f101
Alex1um <assassin20563@gmail.com>
2023-10-18 17:30:51 +0700

    fixed reading data. combined data

--- data.py
+++ data.py
@@ -27,13 +27,39 @@ class Data:
                 for k in self.__annotations__.keys():
                     self.__setattr__(k, list())
                 for row in readed:
+                    if len(row) == 0:
+                        break
                     for (col_name, col_data), v in zip(vars(self).items(), row):
+                        if "\t"*5 in v:
+                            v = v[:v.find("\t")]
                         col_data.append(self.__annotations__[col_name](v))
                 for k, v in vars(self).items():
                     self.__setattr__(k, np.array(v))
+
+    def append(self, data):
+        for sk, sv in vars(self).items():
+            self.__setattr__(sk, np.append(sv, data.__getattribute__(sk)))
+    
+    def to_csv(self, file: str):
+        with open(file, "w") as f:
+            wr = csv.writer(f)
+            wr.writerows(zip(*vars(self).values()))
+
 
     def __repr__(self) -> str:
         return repr(np.array(vars(self).values()))
 
 if __name__ == "__main__":
-    print(Data.from_file("./meteo_data/BAGAN"))
+    # print(Data("./meteo_data/BAGAN.csv"))
+    # print(Data("./1_первая часть сезона/метео/BAGAN"))
+    import glob
+    for f1, f2 in zip(
+        sorted(glob.glob("1_первая часть сезона/метео/*"), key=lambda x: x.split("/")[-1]), 
+        sorted(glob.glob("2_вторая часть сезона/метео/*"), key=lambda x: x.split("/")[-1])
+        ):
+        name = f1.split("/")[-1]
+        print(f1, f2)
+        d1 = Data(f1)
+        d2 = Data(f2)
+        d1.append(d2)
+        d1.to_csv(f"combined/{name}.csv")

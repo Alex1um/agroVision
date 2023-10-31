f4ff63d6ca1e405858a7828db3584be2f014d6b5
Alex1um <assassin20563@gmail.com>
2023-10-31 18:27:48 +0700

    comments + add warning

--- solar.py
+++ solar.py
@@ -29,22 +29,30 @@ class Solar:
     def __init__(self, file_path: str) -> None:
         is_xl = True
         param_count = len(self.__annotations__)
+        # xl same as in data
         try:
             xl = openpyxl.open(file_path, read_only=True, data_only=True)
             sheet: Worksheet = xl.active
             parsed = list()
+            # row_i for logging errors
             for row_i, row in enumerate(sheet.iter_rows(values_only=True)):
                 parsed_row = list()
                 for (name, type), val in zip(self.__annotations__.items(), row):
                     if val: 
+                        # try parse readed value to annotation type
                         try:
                             parsed_row.append(type(val))
                         except Exception as e:
                             logging.warning(f"Exception {e} occuped while parsing {val} to {name} at {row_i} row. Skipping row...")
                             break
                 else:
-                    if len(parsed_row) == param_count:
+                    # check parsed count
+                    parsed_count = len(parsed_row)
+                    if param_count == param_count:
                         parsed.append(parsed_row)
+                    else:
+                        logging.warning(f"parsed not enough params: {parsed_count} != {param_count} at row {row_i}")
+            # to np array
             parsed = np.transpose(np.array(parsed))
             for k, v in zip(self.__annotations__.keys(), parsed):
                 self.__setattr__(k, v)
@@ -53,10 +61,39 @@ class Solar:
             is_xl = False
         except Exception as e:
             logging.error(f"Cnnot parse file {file_path}: {e}")
-        # if not is_xl:
+        # plain text
+        if not is_xl:
+            with open(file_path, "r") as f:
+                # first line = name
+                name = f.readline().strip()
+                # second = mnth/year
+                months = f.readline().strip().split(" " * 4)
+                months_count = len(months)
+                values = [0] * months_count
+                for i in range(months_count):
+                    values[i] = list()
+
+                # read while empty line
+                rl = f.readline().strip()
+                while rl:
+                    parsed_values = map(float, rl.split(" " * 4))
+                    for lst, val in zip(values, parsed_values):
+                        lst.append(val)
+                    rl = f.readline().strip()
+                self.date = list()
+                self.radiation = list()
+                # convert to arrays
+                for month, values in zip(months, values):
+                    for i, val in enumerate(values):
+                        self.date.append(XlNumDate(datetime.strptime(f"{i + 1}/{month}", "%d/%m/%y")))
+                        self.radiation.append(val)
+                self.date = np.array(self.date)
+                self.radiation = np.array(self.radiation)
+
 
     def __repr__(self) -> str:
         return repr(np.array(self.__dict__.values()))
 
 if __name__ == "__main__":
-    print(Solar("2_вторая часть сезона/солнечная радиация/sun_bagan.xlsx"))
+    # print(Solar("2_вторая часть сезона/солнечная радиация/sun_bagan.xlsx"))
+    print(Solar("1_первая часть сезона/солнечная радиация/sun_bagan"))
